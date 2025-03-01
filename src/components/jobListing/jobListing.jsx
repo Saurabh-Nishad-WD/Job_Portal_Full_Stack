9c7c9f8aaa25c8e4afc73136f6f6091f7c29ff5e
saurabh Nishad <saurabhnishad.7651@gmail.com>
2025-03-01 23:13:05 +0530

    vv

---
+++ src/components/jobListing/jobListing.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { Appcontext } from '../../context/AppContext'
+
+const jobListing = () => {
+
+  const {searched,searchFilter} = useContext(Appcontext)
+
+  return (
+    <div>
+      <div className="">
+
+      </div>
+    </div>
+  )
+}
+
+export default jobListing
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
+import jobListing from '../components/jobListing/jobListing'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <jobListing/>
     </div>
   );
 };
